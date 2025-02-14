c7ce2f6c213d9746b0cfe69f8d050a285cd79163
Igor Oliveira Duarte <igor.oliveiraduarte05@gmail.com>
2025-02-14 15:19:41 -0300

    add PONT inserir

--- arvor_struct.cpp
+++ arvor_struct.cpp
@@ -55,18 +55,30 @@ PONT criarNo(int valor) {
 //------------------------------------------------------------------------------
 // 3) Buscar
 PONT buscar(PONT raiz, int valor) {
-  // COMPLETAR
-  // Retorna ponteiro para nó com chave == valor ou NULL se não existir.
-  // Usar a lógica de BST:
-  //  - se valor < raiz->chave => buscar à esquerda
-  //  - se valor > raiz->chave => buscar à direita
-  //  - se valor == raiz->chave => retorna raiz
-  return NULL; // provisório
+    if (raiz==  NULL || raiz-> chave == valor){
+        return raiz;
+    }
+    if (valor < raiz -> chave){
+        return buscar(raiz->esq, valor);
+    }else{
+        return buscar(raiz->dir, valor);
+    }
+  return NULL; 
 }
 
 //------------------------------------------------------------------------------
 // 4) Inserir
 PONT inserir(PONT raiz, int valor) {
+  if(raiz == NULL) {
+    raiz = criarNo(valor);
+  }
+  if (valor< raiz->chave) {
+    raiz->esq== inserir(raiz->esq, valor);
+    }else if (valor > raiz->chave) {
+      raiz->dir = inserir(raiz->dir, valor);
+    }else {
+      raiz->contador++;
+    }
   // COMPLETAR
   // Se raiz == NULL => cria nó
   // Se valor < raiz->chave => raiz->esq = inserir(raiz->esq, valor)
